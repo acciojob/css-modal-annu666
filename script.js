bc5cf71c7c287a2955d81efdec6bb93fb93de8b3
annu666 <148373958+annu666@users.noreply.github.com>
2024-01-24 13:08:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+var modal = document.querySelector('.modal');
+var btn = document.querySelector("#openModal");
+var span = document.querySelector(".close-modal");
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
